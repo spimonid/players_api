dbbc724917720f0e240d4839719d58345134f069
spimonid <33667846+spimonid@users.noreply.github.com>
2022-04-02 16:54:05 -0500

    players controller with show and search actions

---
+++ app/controllers/players_controller.rb
@@ -0,0 +1,21 @@
+class PlayersController < ApplicationController
+  def show
+    @player = Player.find(params[:id])
+
+    render :json => @player.to_json( :except => [:full_name, :sport, :created_at, :updated_at] )
+  end
+
+  def search
+    @result = Player.search(params["player"])
+    feeling_lucky = params["player"].keys.include?("feeling_lucky")
+    result = feeling_lucky ? @result[0] : @result
+
+    render :json => result.to_json( :except => [:full_name, :sport, :created_at, :updated_at] )
+  end
+
+  private
+
+  def permitted_search_params
+    params.permit(:sport, :position, :age, :min_age, :max_age, :first_letter_last_name)
+  end
+end
